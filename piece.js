8c8914b7208e40e3d91708d53024b9fd761eb8ed
Mohammed Alwosaibi <mohammedalwosaibi@Alwosabis-MBP.localdomain>
2023-06-30 12:47:31 -0400

    Add ability for piece to fall and spawn new piece

--- piece.js
+++ piece.js
@@ -2,7 +2,7 @@ const gameBoard = document.querySelector("#game-board")
 
 export class Piece {
     constructor(rotations) {
-        this.rotations = rotations
+        this.rotations = JSON.parse(JSON.stringify(rotations))
         this.currentRotation = 0
         this.tetrominoElems = []
         this.rotations[this.currentRotation].forEach(coord => {
@@ -21,8 +21,10 @@ export class Piece {
 
     draw() {
         this.rotations[this.currentRotation].forEach((coord, index) => {
-            this.tetrominoElems[index].style.gridColumnStart = coord.x
-            this.tetrominoElems[index].style.gridRowStart = coord.y
+            // this.tetrominoElems[index].style.gridColumnStart = coord.x
+            // this.tetrominoElems[index].style.gridRowStart = coord.y
+            this.tetrominoElems[index].style.setProperty("grid-column-start", coord.x)
+            this.tetrominoElems[index].style.setProperty("grid-row-start", coord.y)
         })
     }
 
@@ -68,5 +70,24 @@ export class Piece {
         if (this.rotations[this.currentRotation].some(coord => coord.x < 1)) {
             this.moveRight()
         }
+    }
+
+    atBottom() {
+        let pieceBelow = false
+        Array.from(gameBoard.children).forEach(square => {
+            // console.log(square.style.gridColumnStart)
+            if (this.rotations[this.currentRotation].some(coord => {
+                // console.log(`Current: (${coord.x}, ${coord.y})`)
+                // console.log(`Other: (${square.style.gridColumnStart}, ${square.style.gridRowStart})`)
+                // console.log (`${square.style.gridRowStart - 1 === coord.y} ${square.style.gridColumnStart == coord.x} ${!this.tetrominoElems.includes(square)}`)
+                if (square.style.gridRowStart - 1 === coord.y && parseInt(square.style.gridColumnStart) === coord.x && !this.tetrominoElems.includes(square)) {
+                    // console.log("TESTTEST")
+                    return true
+                }
+            })) {
+                pieceBelow = true
+            }
+        })
+        return this.rotations[this.currentRotation].some(coord => coord.y >= 20) || pieceBelow
     }
 }
--- script.js
+++ script.js
@@ -1,39 +1,70 @@
 import { ROTATIONS } from "./constants.js"
 import { Piece } from "./piece.js"
 
+const GAME_SPEED = 6
+
 function main(currentTime) {
-    update()
+    update(currentTime)
     draw()
+
+    const hue = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--hue"))
+    document.documentElement.style.setProperty("--hue", hue + 0.5)
 
     window.requestAnimationFrame(main)
 }
 
-function update() {
-    const hue = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--hue"))
-    document.documentElement.style.setProperty("--hue", hue + 0.5)
+let currentPiece = new Piece(ROTATIONS[Object.keys(ROTATIONS)[Math.floor(Math.random() * 7)]])
+let nextPieceRotations = ROTATIONS[Object.keys(ROTATIONS)[Math.floor(Math.random() * 7)]]
+
+let lastTime = 0
+
+function update(currentTime) {
+    let delta = currentTime - lastTime
+
+    if (delta < (1 / GAME_SPEED) * 1000) {
+        return
+    }
+
+    // currentPiece.moveDown()
+    // currentPiece.moveDown()
+    // let newPiece = new Piece(nextPieceRotations)
+    // currentPiece.draw()
+    // newPiece.draw()
+    // console.log(newPiece.atBottom())
+
+    if (!currentPiece.atBottom()) {
+        currentPiece.moveDown()
+    } else {
+        currentPiece = new Piece(nextPieceRotations)
+        nextPieceRotations = ROTATIONS[Object.keys(ROTATIONS)[Math.floor(Math.random() * 7)]]
+    }
+
+    lastTime = currentTime
 }
 
-const jPiece = new Piece(ROTATIONS.J)
+// update(1000)
 
 function draw() {
-    jPiece.draw()
+    currentPiece.draw()
 }
 
 window.addEventListener("keydown", event => {
     switch (event.key) {
         case "ArrowRight":
-            jPiece.moveRight()
+            currentPiece.moveRight()
             break
         case "ArrowLeft":
-            jPiece.moveLeft()
+            currentPiece.moveLeft()
             break
         case "ArrowDown":
-            jPiece.moveDown()
+            if (!currentPiece.atBottom()) {
+                currentPiece.moveDown()
+            }
             break
         case "ArrowUp":
-            jPiece.rotate()
+            currentPiece.rotate()
     }
 })
 
-// setInterval(() => jPiece.moveDown(), 1000)
+// setInterval(() => currentPiece.moveDown(), 1000)
 window.requestAnimationFrame(main)
